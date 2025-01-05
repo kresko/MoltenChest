85c717a71a9f6493926dfa5dc4f235181f04bc15
kresko <kresko1995@gmail.com>
2025-01-05 17:52:01 +0100

    [MC-7] - test.

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 require("dotenv").config();
 
 const dbUrl = `${process.env.DATABASE_URL}`;
-const test = '';
 
 module.exports = new Pool({
     connectionString: dbUrl,
